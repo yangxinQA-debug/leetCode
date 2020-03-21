d001842052ff11b43b6368913cf82ce11ceb5d52
琳麟 <linlin.yx@alipay.com>
2020-03-21 15:59:50 +0800

    add 279

--- 279. 完全平方数.py
+++ 279. 完全平方数.py
@@ -8,13 +8,37 @@
 解释: 12 = 4 + 4 + 4.
 示例 2:
 
+
 输入: n = 13
 输出: 2
 解释: 13 = 4 + 9.
 """
+"""
+dp[i] 表示组成和为i的完全平方数的最小个数
+1）初始化dp[0] = 0,dp[1] =1
+2)状态转移方程： dp[i] =i 
+              for j in range(square(i)):
+                  dp[i] = min(dp[i],dp[i-j*j]+1)
+"""
+
+import math
+
+
 class Solution(object):
     def numSquares(self, n):
         """
         :type n: int
         :rtype: int
         """
+        dp = [0] * (n+1)
+        for i in range(n+1):  # 从小到大求dp[i]数组
+            dp[i] = i  # 初始化dp[i] 最多为i个1 组成
+            for j in range(int(math.sqrt(i)+1)):
+                # 更新dp[i]的值，如果dp[i-j*j]+1更新，替换为新值。dp[i-j*j]+1 表示由 j 和组成i-j*j的数，一起组合成i
+                dp[i] = min(dp[i], dp[i - j * j] + 1)
+        print( dp[n])
+        return dp[n]
+
+if __name__ == "__main__":
+    solution = Solution()
+    solution.numSquares(13)
